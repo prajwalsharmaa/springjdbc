8580179880002bff81555136d6af3987f3388037
prajwalsharmaa <prajwalst1213@gmail.com>
2026-05-11 00:38:07 +0545

    mysql database xml configuration

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,13 +1,15 @@
 package com.spring.jdbc;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        JdbcTemplate template =  (JdbcTemplate) context.getBean("jdbcTemplate");
+        System.out.println(template);
     }
 }
