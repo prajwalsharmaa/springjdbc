efa6b7f4f3a7d44cb4e5074b386107fca3b663a3
prajwalsharmaa <prajwalst1213@gmail.com>
2026-05-11 23:54:13 +0545

    jdbc insert query using dao design pattern

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -4,12 +4,33 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.entities.Student;
+
 public class App 
 {
     public static void main( String[] args )
     {
         ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
-        JdbcTemplate template =  (JdbcTemplate) context.getBean("jdbcTemplate");
-        System.out.println(template);
+		/*
+		 * JdbcTemplate template = (JdbcTemplate) context.getBean("jdbcTemplate");
+		 * System.out.println("Jdbc Program Started");
+		 * 
+		 * //Insert Query, String query =
+		 * "insert into student(id,name,city) values(?,?,?)";
+		 * 
+		 * //fire query int result =
+		 * template.update(query,456,"Prajwal Sharma","Bhairahawa");
+		 * System.out.println("number of record inserted.."+result);
+		 */
+        StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
+        
+        Student student = new Student();
+        student.setId(344);
+        student.setCity("Bhaktapurii");
+        student.setName("Ramesh Budhathoki");
+        
+        int result = studentDao.insert(student);
+        System.out.println("student added"+ result);
     }
 }
